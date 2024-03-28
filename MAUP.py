591e4a052b8631e53285f36ce9582a052ce19678
againTraviscodes <t.uni.versity17@gmail.com>
2024-03-27 22:26:50 -0700

    print debugs

--- MAUP.py
+++ MAUP.py
@@ -9,8 +9,6 @@ Lab 3: MAUP and data.  See details on Canvas page
 
 Make sure to say where/when you got your data!
 """
-import string
-
 import pandas as pd
 import geopandas as gpd
 import maup
@@ -22,14 +20,15 @@ maup.progress.enabled = True
 
 start_time = time.time()
 
-# la_graph = Graph.from_json("./LA/LA.geojson")
+print("Starting graph load")
+# la_graph = Graph.from_json("./LA/LA.geojson")  # node issue
 la_graph = Graph.from_file("./LA/LA.shp")
 print("Graph loaded")
 initial_partition = init_partition(la_graph)
 print("Initial partition initialized")
 ideal_pop = calc_population(la_graph, 6, "TOTPOP")
 print("Ideal population: ", ideal_pop)
-rand_walk = init_markov_chain(la_graph, initial_partition, "TOTPOP", ideal_pop, 2000)
+rand_walk = init_markov_chain(la_graph, initial_partition, "TOTPOP", ideal_pop, 3)  # 2000
 print("Markov Chain initialized")
 cutedge_ensemble = []
 lmaj_ensemble = []
@@ -38,11 +37,11 @@ cutedge_ensemble, lmaj_ensemble, dem_win_ensemble = walk_the_run(rand_walk, 6, c
 
 # Histograms
 # 1. Cut edge
-plot_histograms(cutedge_ensemble, "cutedge_ensemble.png")
+plot_histograms(cutedge_ensemble, "histograms/cutedge_ensemble.png")
 # 2. Majority-Latino districts
-plot_histograms(lmaj_ensemble, "lmaj_ensemble.png")
+plot_histograms(lmaj_ensemble, "histograms/lmaj_ensemble.png")
 # 3. Democratic-won districts
-plot_histograms(dem_win_ensemble, "dem_win_ensemble.png")
+plot_histograms(dem_win_ensemble, "histograms/dem_win_ensemble.png")
 print("Histograms writen to files")
 
 end_time = time.time()
