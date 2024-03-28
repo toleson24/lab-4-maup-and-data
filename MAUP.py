154bbba217dcb7d45e4dfa07cb92f6665d608d7a
againTraviscodes <t.uni.versity17@gmail.com>
2024-03-27 23:32:44 -0700

    usage of RedistrictingMarkovChain.py implemented

--- MAUP.py
+++ MAUP.py
@@ -14,7 +14,7 @@ import geopandas as gpd
 import maup
 import time
 
-from gerrychain_functions import *
+from RedistrictingMarkovChain import *
 
 maup.progress.enabled = True
 
@@ -24,16 +24,17 @@ print("Starting graph load")
 # la_graph = Graph.from_json("./LA/LA.geojson")  # node issue
 la_graph = Graph.from_file("./LA/LA.shp")
 print("Graph loaded")
-initial_partition = init_partition(la_graph)
-print("Initial partition initialized")
-ideal_pop = calc_population(la_graph, 6, "TOTPOP")
-print("Ideal population: ", ideal_pop)
-rand_walk = init_markov_chain(la_graph, initial_partition, "TOTPOP", ideal_pop, 100)  # 2000
-print("Markov Chain initialized")
-cutedge_ensemble = []
-lmaj_ensemble = []
-dem_win_ensemble = []
-cutedge_ensemble, lmaj_ensemble, dem_win_ensemble = walk_the_run(rand_walk, 6, "TOTPOP", cutedge_ensemble, lmaj_ensemble, dem_win_ensemble)
+la_markov_chain = RedistrictingMarkovChain(la_graph,
+                                           6,
+                                           "CD",
+                                           "PRES20",
+                                           "G20PRED",
+                                           "G20PRER",
+                                           "TOTPOP",
+                                           "HISP")
+la_markov_chain.init_partition()
+la_markov_chain.init_markov_chain(steps=1000)
+cutedge_ensemble, lmaj_ensemble, dem_win_ensemble = la_markov_chain.walk_the_run()
 
 # Histograms
 # 1. Cut edge
@@ -42,7 +43,6 @@ plot_histograms(cutedge_ensemble, "histograms/cutedge_ensemble.png")
 plot_histograms(lmaj_ensemble, "histograms/lmaj_ensemble.png")
 # 3. Democratic-won districts
 plot_histograms(dem_win_ensemble, "histograms/dem_win_ensemble.png")
-print("Histograms writen to files")
 
 end_time = time.time()
 print("The time of execution of above program is :",
